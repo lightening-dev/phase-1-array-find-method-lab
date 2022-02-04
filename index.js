bc1b1f444ce163cfa4f4530910f160a5deafcaab
Lightening Gibson <lighteningibson@gmail.com>
2022-02-03 21:44:39 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function superbowlWin(record){
+
+    const found = record.find(function(game){
+        return game.result === "W"
+    
+    })
+        
+    if (found) {
+        
+            return found.year; 
+    }
+
+    
+}
+
