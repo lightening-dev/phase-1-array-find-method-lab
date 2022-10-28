f72a9cb5e7e96436032f3bcacbf7dc6005f43929
Lightening Gibson <lighteningibson@gmail.com>
2022-10-28 10:22:07 -0500

    commit tree 2a7988e2968a8ddc15b85e6b3f3837dd51fb5b09

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 // code your solution here
 
-function superbowlWin(record){
 
-    const found = record.find(function(game){
-        return game.result === "W"
-    
-    })
-        
-    if (found) {
-        
-            return found.year; 
-    }
 
-    
+
+
+// we want to create a call back function that will evaluate if each result of each object is a win
+
+function superbowlWin(array) {
+   //const found = array1.find(element => element > 10);
+
+   let found = array.find(obj => obj.result === "W")
+   console.log(found);
+
+   if(found) {
+    return found.year;
+   }
 }
+
+// record.find(superbowlWin).year
+
 
